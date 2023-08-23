a739342ac13556878f1452a377f6e413b6e4ef7a
WilliamNguyen <dungnguyen260103@gmail.com>
2023-08-23 21:46:10 +0700

    + Game 3

---
+++ Assets/_Game/Dung_Scripts/Game3/CleanerCtrl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class CleanerCtrl : MonoBehaviour
+{
+    Transform tf;
+    [SerializeField] private Camera mainCamera;
+
+    private void Start()
+    {
+        tf = transform;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitInfo;
+
+                if (Physics.Raycast(ray, out hitInfo))
+                {
+                    Vector3 targetPosition = hitInfo.point;
+                    targetPosition.z = tf.position.z;
+                    tf.position = Vector3.Lerp( tf.position,targetPosition, 0.3f);
+
+                }
+            }
+        }
+    }
+}
